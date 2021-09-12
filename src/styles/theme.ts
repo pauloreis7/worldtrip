7f1180cc5bc0c81df7caf5c381ab7f14bc95281a
Paulo Reis <paulosilvadosreis2057@gmail.com>
2021-09-12 20:12:17 -0300

    Build initial project structure with typescript, next and chakraui

---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      "900": "47585B",
+      "600": "999999",
+      "400": "DADADA",
+      "200": "F5F8FA",
+    }
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'white',
+        color: 'gray.900'
+      },
+    },
+  }
+})
